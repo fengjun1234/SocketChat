152d509223c6e2f4877e3fafc48fda2356a3bc68
fengjun1234 <fj_fengjun@126.com>
2016-06-02 13:39:46 +0800

    Initial Commit

---
+++ 6-socket基础/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  6-socket基础
+//
+//  Created by fengjun on 16/6/2.
+//  Copyright © 2016年 fengjun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 6-socket基础/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  6-socket基础
+//
+//  Created by fengjun on 16/6/2.
+//  Copyright © 2016年 fengjun. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
